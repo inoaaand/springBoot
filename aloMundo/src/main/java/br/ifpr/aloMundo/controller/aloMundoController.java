31b6f948b6db916ce2b972e129581ae839601820
fer <20233005420@estudantes.ifpr.edu.br>
2025-08-19 14:37:27 -0300

    funfou

--- aloMundo/src/main/java/br/ifpr/aloMundo/controller/aloMundoController.java
+++ aloMundo/src/main/java/br/ifpr/aloMundo/controller/aloMundoController.java
@@ -1,4 +1,8 @@
-package main.java.br.ifpr.aloMundo.controller;
+package br.ifpr.aloMundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
