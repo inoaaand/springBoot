3bbc18b65d2e8715b37a4b4bc72391b11bab30a2
fer <20233005420@estudantes.ifpr.edu.br>
2025-08-25 08:14:25 -0300

    implementações classe erro

--- aloMundo/src/main/java/br/ifpr/aloMundo/erros/GlobalExceptionHandler.java
+++ aloMundo/src/main/java/br/ifpr/aloMundo/erros/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.ifpr.aloMundo.erros;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handle(Exception e){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERRO:" + e.getMessage());
+    } 
+
+    //outra forma de tratar erros
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle02(Exception e){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "https://http.cat/404.jpg");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     } 
 
 }
